fddec95f265a6ae1dc831f6dd923a0cf2dc74712
nazephyrus <126683488+naden01@users.noreply.github.com>
2025-04-19 08:15:28 -0700

    BG7n

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PATH := device/tecno/TECNO-BG7n
+DEVICE_PATH := device/tecno/BG7n
 
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
